00422f3944fde2cbe27a9c49b191abcc8193cf22
AnOzen <trevore.gene901@gmail.com>
2026-02-24 22:01:47 -0600

    Added wasd movement

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,27 +1,59 @@
 // TODO: Pixi Client
 
-import { Application } from "pixi.js";
+import { Application, Container, Point } from "pixi.js";
 import State from "./util/state";
 
+const TRAVEL = 10;
+
+let heldkeys: string[] = [];
+
 (async () => {
-  const app: Application = new Application();
+	const app: Application = new Application();
 
-  await app.init({
-    background: "#FFFFFF",
-    resizeTo: window,
-    resolution: 1,
-    preference: "webgl",
-    antialias: true,
-  });
+	await app.init({
+		background: "#FFFFFF",
+		resizeTo: window,
+		resolution: 1,
+		preference: "webgl",
+		antialias: true,
+	});
 
-  document.body.appendChild(app.canvas);
+	document.body.appendChild(app.canvas);
 
-  let st1 = new State(0, 0, 250, 20);
-  let st2 = new State(1, 0, 250, 20);
+	let tree = new Container();
 
-  app.stage.addChild(st1);
-  app.stage.addChild(st2);
+	app.stage.addChild(tree);
 
+	tree.addChild(new State(0, 0, 100, 30));
 
-  
+	onkeydown = (e) => {
+		if (e.repeat) return;
+		heldkeys.push(e.key);
+	};
+
+	onkeyup = (e) => {
+		let i = heldkeys.indexOf(e.key);
+		if (i > -1) {
+			heldkeys.splice(i, 1);
+		}
+	};
+
+	app.ticker.add((time) => {
+		heldkeys.forEach((s) => {
+			switch (s) {
+				case "w":
+					tree.position.set(tree.position._x, tree.position._y - TRAVEL);
+					break;
+				case "s":
+					tree.position.set(tree.position._x, tree.position._y + TRAVEL);
+					break;
+				case "a":
+					tree.position.set(tree.position._x - TRAVEL, tree.position._y);
+					break;
+				case "d":
+					tree.position.set(tree.position._x + TRAVEL, tree.position._y);
+					break;
+			}
+		});
+	});
 })();
--- client/src/util/state.ts
+++ client/src/util/state.ts
@@ -1,25 +1,25 @@
 import { Container, Graphics, Size } from "pixi.js";
 
 export default class State extends Container {
-  graphics: Graphics;
-  size: number;
-  margin: number;
+	graphics: Graphics;
+	size: number;
+	margin: number;
 
-  constructor(x: number, y: number, size: number, margin: number) {
-    super();
-    this.graphics = new Graphics();
-    this.size = size;
-    this.margin = margin;
+	constructor(x: number, y: number, size: number, margin: number) {
+		super();
+		this.graphics = new Graphics();
+		this.size = size;
+		this.margin = margin;
 
-    this.addChild(this.graphics.rect(0, 0, size, size).fill("red"));
+		this.addChild(this.graphics.rect(0, 0, size, size).fill("red"));
 
-    this.pos(x, y);
-  }
+		this.pos(x, y);
+	}
 
-  pos(x: number, y: number): void {
-    this.position.set(
-      x * (this.size + this.margin),
-      y * (this.size + this.margin),
-    );
-  }
+	pos(x: number, y: number): void {
+		this.position.set(
+			x * (this.size + this.margin),
+			y * (this.size + this.margin),
+		);
+	}
 }
